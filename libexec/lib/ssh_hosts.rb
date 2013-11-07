796e719fadf856849d0bf8d1f01e6f7676b4e869
DEkart <rene.dekart@gmail.com>
2013-11-07 16:59:10 +0600

    Allow to set rails folder in host configuration file

--- libexec/lib/ssh_hosts.rb
+++ libexec/lib/ssh_hosts.rb
@@ -1,3 +1,5 @@
+require "yaml"
+
 module SshHosts
   class << self
     def host_list_autocomplete
@@ -8,8 +10,22 @@ module SshHosts
 
     def remote_command(server, command)
       system %{
-        ssh #{server} -t "bash -i -c '#{ command }'"
+        ssh #{server} -t "bash -i -c '. ~/.profile; #{ command }'"
       }
+    end
+
+    def remote_rails_folder(server)
+      config_file = Dir[File.expand_path("~/.ssh/hosts/**/#{server}.yml")].first
+
+      if File.file?(config_file)
+        config = YAML.load_file(config_file)[server]
+
+        if config['de'] and config['de']['rails_folder']
+          return config['de']['rails_folder']
+        end
+      end
+
+      server
     end
   end
 end
